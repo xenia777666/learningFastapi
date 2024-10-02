d43ba0d108fd3f4ffbdc0c8e661b1614a57163b5
jsxenix <143598349+xenia777666@users.noreply.github.com>
2024-10-03 02:43:09 +0300

    add files

--- database.py
+++ database.py
@@ -1,12 +1,17 @@
-from sqlalchemy.ext.asyncio import create_async_engine
-    AsyncSession  # из расширений библиотеки для бд берем асинхронный движок
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+ # из расширений библиотеки для бд берем асинхронный движок
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base, Mapped, mapped_column
 
 engine = create_async_engine(
    "sqlite+aiosqlite:///tasks.db") #юрл бд+назва бд+драйвер+назв файла)
 
-new_session = async.sessionmaker(engine, expire_on_commit=False )#фабрика создания сессий, открытие трнзакций для создания бд + параметр
+new_session = async.sessionmaker(engine, expire_on_commit=False )
+# Здесь мы создаем асинхронное подключение, которое будет отвечать за отправку запросов в базу
+# данных engine. Обратите внимание, что мы говорим SQLAlchemy, что будем использовать
+# драйвер для асинхронного кода aiosqlite. После создания engine, с которым уже можно работать,
+# мы дополнительно создаем фабрику сессий new_session. Сессия позволяет
+# работать не с обычными списками и словарями, а с моделями данных, которые создаются через классы
 
 class Model(DeclarativeBase):
    pass
@@ -16,3 +21,11 @@ class TaskTable(Model)#таблица
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
     description: Mapped[str | None]
+
+async def create_tables():
+   async with engine.begin() as conn:
+       await conn.run_sync(Model.metadata.create_all)
+
+async def delete_tables():
+   async with engine.begin() as conn:
+       await conn.run_sync(Model.metadata.drop_all)
